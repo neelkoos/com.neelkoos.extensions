5de21e9ccbc1d57c1005de4b431fbe6172bc846d
neelkoos <kosa.kornel@gmail.com>
2021-12-06 11:48:22 +0000

    +gitIgnore add meta files +ShowOnlyDrawer
    ShowOnly attribute for ReadOnly inspector properties

---
+++ Editor/ShowOnlyDrawer.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Neelkoos.Extensions.Editor
+{
+    public class ShowOnlyAttribute : PropertyAttribute
+    {
+
+    }
+    
+    [CustomPropertyDrawer(typeof(ShowOnlyAttribute))]
+    public class ShowOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+        {
+            var valueStr = prop.propertyType switch
+            {
+                SerializedPropertyType.Integer => prop.intValue.ToString(),
+                SerializedPropertyType.Boolean => prop.boolValue.ToString(),
+                SerializedPropertyType.Float => prop.floatValue.ToString("0.00000"),
+                SerializedPropertyType.String => prop.stringValue,
+                _ => "(not supported)"
+            };
+
+            EditorGUI.LabelField(position, label.text, valueStr);
+        }
+    }
+}
+
+
